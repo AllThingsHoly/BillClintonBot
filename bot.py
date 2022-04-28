0c2cc98cc37a7070a54bf542ec10a1e947439c20
DeeDeeAich <32936305+DeeDeeAich@users.noreply.github.com>
2022-04-27 19:48:28 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,3 +2,15 @@ from discord.ext import commands
 import os
 import dotenv
 
+dotenv.load_dotenv("keys.env")
+DISCORD_KEY = os.getenv("DISCORD_KEY")
+
+bot = commands.Bot("!", description="Music Bot")
+
+@bot.event
+async def on_ready():
+    print("Ready")
+    
+@bot.command(pass_context=True)
+async def hello(ctx):
+    await ctx.send("hello!")
